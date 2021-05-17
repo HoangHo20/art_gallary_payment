947eaba14bbf7fb42fcc656730eb6e0b46385525
Ho Duy <khanhduy8@gmail.com>
2021-05-18 03:24:57 +0700

    start menu GUI

--- src/DAO/Global_DAO.java
+++ src/DAO/Global_DAO.java
@@ -5,10 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Global_DAO {
+    private static Connection conn = null;
 
     public static Connection getConnection(){
-        Connection conn = null;
+        if (conn != null) {
+            return conn;
+        } else {
+            connect_to_database();
+            return conn;
+        }
+    }
 
+    private static void connect_to_database() {
         try{
             String dbURL = "jdbc:mysql://localhost/artGallery";
             String username = "root";
@@ -16,9 +24,6 @@ public class Global_DAO {
             conn = DriverManager.getConnection(dbURL, username, password);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }
-        finally {
-            return conn;
         }
     }
 }
